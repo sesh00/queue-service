10633a4054fca6c137615fbb3d8fc2f29961b68b
sesh00 <ernestrsage@gmail.com>
2023-12-14 11:06:54 +0300

    initial commit

---
+++ src/main/java/com/github/sesh00/queueservice/QueueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.sesh00.queueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueueServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/sesh00/queueservice/QueueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sesh00.queueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
